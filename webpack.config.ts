a71bf676fc51efe95b9c01be9a52e9c9d435e250
Caique Lourenco <caique.moreira@edglobo.com.br>
2022-07-28 16:28:10 -0300

    Configurou Webpack com Babel Eslint e Typescript

---
+++ webpack.config.ts
@@ -0,0 +1,70 @@
+import path from 'path';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+
+const config = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'tiny.js',
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    host: 'tinyjs.globoi.com',
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+        // sourceMap: true
+      }),
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
+  devtool: 'inline-source-map',
+};
+
+export default config;
